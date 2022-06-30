bf7e2be5d45eb527b26f605e7842cb39f5adf682
slogup-dale <dyddn304@gmail.com>
2022-06-30 14:28:09 +0900

    add: 초기세팅 완료

--- src/page/Main/index.tsx
+++ src/page/Main/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import Footer from "../../components/Layout/Footer";
-import Header from "../../components/Layout/Header";
+import React from 'react';
+import Footer from '../../components/Layout/Footer';
+import Header from '../../components/Layout/Header';
 
 const Main = () => {
   return (
