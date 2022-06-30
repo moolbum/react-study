bd1bbad47d7edc0266bffa3644c87351b3076109
slogup-dale <dyddn304@gmail.com>
2022-06-30 13:46:30 +0900

    add: 초기세팅

---
+++ src/page/Main/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../../components/Layout/Footer";
+import Header from "../../components/Layout/Header";
+
+const Main = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default Main;
